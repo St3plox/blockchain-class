bdbef75043ae018a813a93c859ddf8b2d7eb068e
St3plox <st3pegor@gmail.com>
2024-05-06 15:33:39 +0700

    scratch digital sig

--- app/scratch/main.go
+++ app/scratch/main.go
@@ -1,5 +1,50 @@
 package main
 
-func main() {
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/ethereum/go-ethereum/common/hexutil"
+	"github.com/ethereum/go-ethereum/crypto"
+	"log"
+)
+
+type Tx struct {
+	FromID  string `json:"from"`     // Ethereum: Account sending the transaction. Will be checked against signature.
+	ToID    string `json:"to"`       // Ethereum: Account receiving the benefit of the transaction.
+	Value   uint64    `json:"value"`    // Ethereum: Monetary value received from this transaction.
+}
+
+func main()  {
+	if err := run(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func run() error{
+
+	tx := Tx{
+		FromID: "Soser",
+		ToID:   "Sosun",
+		Value:  1000,
+	}
+
+	privKey, err := crypto.LoadECDSA("zblock/accounts/kennedy.ecdsa")
+	if err != nil {
+		return fmt.Errorf("Unable to load privKey %w", err)
+	}
+
+	data, err := json.Marshal(tx)
+	if err != nil {
+		return err
+	}
 	
+	v := crypto.Keccak256(data)
+
+	sig, err := crypto.Sign(v, privKey)
+	if err != nil {
+		return err
+	}
+	fmt.Println(hexutil.Encode(sig))
+
+	return nil
 }
